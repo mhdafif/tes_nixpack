3dbb880b2cabd969a76c132fccf1970df0ccb500
Afif <hammadaf9408@gmail.com>
2026-01-05 12:43:39 +0700

    adjust

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone", // Build standalone server bundle (for Nixpacks/containers)
   /* config options here */
 };
 
